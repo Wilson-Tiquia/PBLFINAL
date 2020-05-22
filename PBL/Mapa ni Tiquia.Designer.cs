4c66ceb3491c0e3fc1da1be9b16fad5ea676b933
jwtiquia@gmail.com <jwtiquia@gmail.com>
2020-05-23 05:02:05 +0800

    so i pplus n lng yung problema ko

--- PBL/Mapa ni Tiquia.Designer.cs
+++ PBL/Mapa ni Tiquia.Designer.cs
@@ -42,14 +42,30 @@
             this.regionMapComboBox = new System.Windows.Forms.ComboBox();
             this.provinceMapComboBox = new System.Windows.Forms.ComboBox();
             this.panelOfImage = new System.Windows.Forms.Panel();
-            this.currentIsland = new System.Windows.Forms.Label();
+            this.tCase = new System.Windows.Forms.Label();
+            this.provinceName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.range501To1k = new System.Windows.Forms.Label();
+            this.range0To500 = new System.Windows.Forms.Label();
+            this.case1 = new System.Windows.Forms.PictureBox();
+            this.case2 = new System.Windows.Forms.PictureBox();
+            this.case3 = new System.Windows.Forms.PictureBox();
+            this.case4 = new System.Windows.Forms.PictureBox();
             this.backButton = new System.Windows.Forms.Button();
             this.provincePicture = new System.Windows.Forms.PictureBox();
+            this.currentIsland = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.mindanao = new System.Windows.Forms.Button();
             this.visayas = new System.Windows.Forms.Button();
             this.luzonButton = new System.Windows.Forms.Button();
+            this.provinceslistbox = new System.Windows.Forms.ListBox();
+            this.caseListBox = new System.Windows.Forms.ListBox();
             this.panelOfImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.case1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.case2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.case3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.case4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.provincePicture)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -180,27 +196,127 @@
             // panelOfImage
             // 
             this.panelOfImage.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelOfImage.Controls.Add(this.tCase);
+            this.panelOfImage.Controls.Add(this.provinceName);
+            this.panelOfImage.Controls.Add(this.label2);
+            this.panelOfImage.Controls.Add(this.label1);
+            this.panelOfImage.Controls.Add(this.range501To1k);
+            this.panelOfImage.Controls.Add(this.range0To500);
+            this.panelOfImage.Controls.Add(this.case1);
+            this.panelOfImage.Controls.Add(this.case2);
+            this.panelOfImage.Controls.Add(this.case3);
+            this.panelOfImage.Controls.Add(this.case4);
             this.panelOfImage.Controls.Add(this.backButton);
             this.panelOfImage.Controls.Add(this.provincePicture);
-            this.panelOfImage.Location = new System.Drawing.Point(110, 420);
+            this.panelOfImage.Location = new System.Drawing.Point(63, 420);
             this.panelOfImage.Name = "panelOfImage";
-            this.panelOfImage.Size = new System.Drawing.Size(632, 401);
+            this.panelOfImage.Size = new System.Drawing.Size(679, 401);
             this.panelOfImage.TabIndex = 1;
             // 
-            // currentIsland
+            // tCase
             // 
-            this.currentIsland.AutoSize = true;
-            this.currentIsland.BackColor = System.Drawing.Color.DarkOrange;
-            this.currentIsland.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.currentIsland.Location = new System.Drawing.Point(682, 9);
-            this.currentIsland.Name = "currentIsland";
-            this.currentIsland.Size = new System.Drawing.Size(332, 40);
-            this.currentIsland.TabIndex = 0;
-            this.currentIsland.Text = "CURRENT ISLAND";
+            this.tCase.AutoSize = true;
+            this.tCase.ForeColor = System.Drawing.Color.Yellow;
+            this.tCase.Location = new System.Drawing.Point(350, 78);
+            this.tCase.Name = "tCase";
+            this.tCase.Size = new System.Drawing.Size(93, 20);
+            this.tCase.TabIndex = 5;
+            this.tCase.Text = "Total Case: ";
+            this.tCase.Click += new System.EventHandler(this.tCase_Click);
+            // 
+            // provinceName
+            // 
+            this.provinceName.AutoSize = true;
+            this.provinceName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.provinceName.Location = new System.Drawing.Point(350, 18);
+            this.provinceName.Name = "provinceName";
+            this.provinceName.Size = new System.Drawing.Size(110, 20);
+            this.provinceName.TabIndex = 4;
+            this.provinceName.Text = "provinceName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(275, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2000+";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(169, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1000-2000";
+            // 
+            // range501To1k
+            // 
+            this.range501To1k.AutoSize = true;
+            this.range501To1k.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.range501To1k.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.range501To1k.Location = new System.Drawing.Point(84, 258);
+            this.range501To1k.Name = "range501To1k";
+            this.range501To1k.Size = new System.Drawing.Size(79, 22);
+            this.range501To1k.TabIndex = 3;
+            this.range501To1k.Text = "501-1000";
+            // 
+            // range0To500
+            // 
+            this.range0To500.AutoSize = true;
+            this.range0To500.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.range0To500.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.range0To500.Location = new System.Drawing.Point(12, 258);
+            this.range0To500.Name = "range0To500";
+            this.range0To500.Size = new System.Drawing.Size(52, 22);
+            this.range0To500.TabIndex = 3;
+            this.range0To500.Text = "0-500";
+            // 
+            // case1
+            // 
+            this.case1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("case1.BackgroundImage")));
+            this.case1.Location = new System.Drawing.Point(12, 201);
+            this.case1.Name = "case1";
+            this.case1.Size = new System.Drawing.Size(49, 36);
+            this.case1.TabIndex = 2;
+            this.case1.TabStop = false;
+            // 
+            // case2
+            // 
+            this.case2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("case2.BackgroundImage")));
+            this.case2.Location = new System.Drawing.Point(98, 201);
+            this.case2.Name = "case2";
+            this.case2.Size = new System.Drawing.Size(49, 36);
+            this.case2.TabIndex = 2;
+            this.case2.TabStop = false;
+            // 
+            // case3
+            // 
+            this.case3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("case3.BackgroundImage")));
+            this.case3.Location = new System.Drawing.Point(186, 201);
+            this.case3.Name = "case3";
+            this.case3.Size = new System.Drawing.Size(49, 36);
+            this.case3.TabIndex = 2;
+            this.case3.TabStop = false;
+            // 
+            // case4
+            // 
+            this.case4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("case4.BackgroundImage")));
+            this.case4.Location = new System.Drawing.Point(275, 201);
+            this.case4.Name = "case4";
+            this.case4.Size = new System.Drawing.Size(49, 36);
+            this.case4.TabIndex = 2;
+            this.case4.TabStop = false;
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(107, 304);
+            this.backButton.Location = new System.Drawing.Point(398, 311);
             this.backButton.Name = "backButton";
             this.backButton.Size = new System.Drawing.Size(201, 64);
             this.backButton.TabIndex = 1;
@@ -212,11 +328,22 @@
             // 
             this.provincePicture.BackColor = System.Drawing.Color.SlateGray;
             this.provincePicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.provincePicture.Location = new System.Drawing.Point(51, 38);
+            this.provincePicture.Location = new System.Drawing.Point(98, 18);
             this.provincePicture.Name = "provincePicture";
             this.provincePicture.Size = new System.Drawing.Size(193, 160);
             this.provincePicture.TabIndex = 0;
             this.provincePicture.TabStop = false;
+            // 
+            // currentIsland
+            // 
+            this.currentIsland.AutoSize = true;
+            this.currentIsland.BackColor = System.Drawing.Color.DarkOrange;
+            this.currentIsland.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentIsland.Location = new System.Drawing.Point(682, 9);
+            this.currentIsland.Name = "currentIsland";
+            this.currentIsland.Size = new System.Drawing.Size(332, 40);
+            this.currentIsland.TabIndex = 0;
+            this.currentIsland.Text = "CURRENT ISLAND";
             // 
             // panel1
             // 
@@ -268,11 +395,31 @@
             this.luzonButton.UseVisualStyleBackColor = false;
             this.luzonButton.Click += new System.EventHandler(this.luzonButton_Click);
             // 
+            // provinceslistbox
+            // 
+            this.provinceslistbox.FormattingEnabled = true;
+            this.provinceslistbox.ItemHeight = 20;
+            this.provinceslistbox.Location = new System.Drawing.Point(668, 108);
+            this.provinceslistbox.Name = "provinceslistbox";
+            this.provinceslistbox.Size = new System.Drawing.Size(286, 324);
+            this.provinceslistbox.TabIndex = 12;
+            // 
+            // caseListBox
+            // 
+            this.caseListBox.FormattingEnabled = true;
+            this.caseListBox.ItemHeight = 20;
+            this.caseListBox.Location = new System.Drawing.Point(978, 108);
+            this.caseListBox.Name = "caseListBox";
+            this.caseListBox.Size = new System.Drawing.Size(286, 324);
+            this.caseListBox.TabIndex = 13;
+            // 
             // Mapa_ni_Tiquia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.caseListBox);
+            this.Controls.Add(this.provinceslistbox);
             this.Controls.Add(this.currentIsland);
             this.Controls.Add(this.panelOfImage);
             this.Controls.Add(this.provinceMapComboBox);
@@ -292,6 +439,11 @@
             this.Text = "Mapa_ni_Tiquia";
             this.Load += new System.EventHandler(this.Mapa_ni_Tiquia_Load);
             this.panelOfImage.ResumeLayout(false);
+            this.panelOfImage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.case1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.case2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.case3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.case4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.provincePicture)).EndInit();
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -320,5 +472,17 @@
         private System.Windows.Forms.PictureBox provincePicture;
         private System.Windows.Forms.Button visayas;
         private System.Windows.Forms.Button mindanao;
+        private System.Windows.Forms.PictureBox case1;
+        private System.Windows.Forms.PictureBox case2;
+        private System.Windows.Forms.PictureBox case3;
+        private System.Windows.Forms.PictureBox case4;
+        private System.Windows.Forms.Label provinceName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label range501To1k;
+        private System.Windows.Forms.Label range0To500;
+        private System.Windows.Forms.Label tCase;
+        private System.Windows.Forms.ListBox provinceslistbox;
+        private System.Windows.Forms.ListBox caseListBox;
     }
 }
