af1422b22624eb06252dd2e1da951122fd5edb38
jwtiquia@gmail.com <jwtiquia@gmail.com>
2020-05-22 20:05:46 +0800

    So nag wrwriteline na sya

--- PBL/Mapa ni Tiquia.Designer.cs
+++ PBL/Mapa ni Tiquia.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mapa_ni_Tiquia));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mapImage = new System.Windows.Forms.PictureBox();
+            this.islandLabel = new System.Windows.Forms.Label();
+            this.regionLabel = new System.Windows.Forms.Label();
+            this.provinceLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.islandComboBox = new System.Windows.Forms.ComboBox();
+            this.regionComboBox = new System.Windows.Forms.ComboBox();
+            this.provinceComboBox = new System.Windows.Forms.ComboBox();
+            this.caseInputTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.mapImage);
+            this.panel1.Location = new System.Drawing.Point(1070, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(766, 569);
+            this.panel1.TabIndex = 0;
+            // 
+            // mapImage
+            // 
+            this.mapImage.Image = ((System.Drawing.Image)(resources.GetObject("mapImage.Image")));
+            this.mapImage.Location = new System.Drawing.Point(218, 27);
+            this.mapImage.Name = "mapImage";
+            this.mapImage.Size = new System.Drawing.Size(515, 505);
+            this.mapImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mapImage.TabIndex = 0;
+            this.mapImage.TabStop = false;
+            // 
+            // islandLabel
+            // 
+            this.islandLabel.AutoSize = true;
+            this.islandLabel.BackColor = System.Drawing.Color.Khaki;
+            this.islandLabel.Location = new System.Drawing.Point(106, 48);
+            this.islandLabel.Name = "islandLabel";
+            this.islandLabel.Size = new System.Drawing.Size(52, 20);
+            this.islandLabel.TabIndex = 1;
+            this.islandLabel.Text = "Island";
+            // 
+            // regionLabel
+            // 
+            this.regionLabel.AutoSize = true;
+            this.regionLabel.BackColor = System.Drawing.Color.Khaki;
+            this.regionLabel.Location = new System.Drawing.Point(106, 145);
+            this.regionLabel.Name = "regionLabel";
+            this.regionLabel.Size = new System.Drawing.Size(60, 20);
+            this.regionLabel.TabIndex = 2;
+            this.regionLabel.Text = "Region";
+            // 
+            // provinceLabel
+            // 
+            this.provinceLabel.AutoSize = true;
+            this.provinceLabel.BackColor = System.Drawing.Color.Khaki;
+            this.provinceLabel.Location = new System.Drawing.Point(106, 246);
+            this.provinceLabel.Name = "provinceLabel";
+            this.provinceLabel.Size = new System.Drawing.Size(69, 20);
+            this.provinceLabel.TabIndex = 1;
+            this.provinceLabel.Text = "Province";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Khaki;
+            this.label4.Location = new System.Drawing.Point(106, 329);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Cases";
+            // 
+            // islandComboBox
+            // 
+            this.islandComboBox.FormattingEnabled = true;
+            this.islandComboBox.Location = new System.Drawing.Point(110, 89);
+            this.islandComboBox.Name = "islandComboBox";
+            this.islandComboBox.Size = new System.Drawing.Size(445, 28);
+            this.islandComboBox.TabIndex = 3;
+            this.islandComboBox.DropDown += new System.EventHandler(this.islandComboBox_DropDown);
+            this.islandComboBox.SelectedIndexChanged += new System.EventHandler(this.islandComboBox_SelectedIndexChanged);
+            // 
+            // regionComboBox
+            // 
+            this.regionComboBox.FormattingEnabled = true;
+            this.regionComboBox.Location = new System.Drawing.Point(110, 180);
+            this.regionComboBox.Name = "regionComboBox";
+            this.regionComboBox.Size = new System.Drawing.Size(445, 28);
+            this.regionComboBox.TabIndex = 4;
+            this.regionComboBox.DropDown += new System.EventHandler(this.regionComboBox_DropDown);
+            this.regionComboBox.SelectedIndexChanged += new System.EventHandler(this.regionComboBox_SelectedIndexChanged);
+            // 
+            // provinceComboBox
+            // 
+            this.provinceComboBox.FormattingEnabled = true;
+            this.provinceComboBox.Location = new System.Drawing.Point(110, 278);
+            this.provinceComboBox.Name = "provinceComboBox";
+            this.provinceComboBox.Size = new System.Drawing.Size(445, 28);
+            this.provinceComboBox.TabIndex = 5;
+            this.provinceComboBox.SelectedIndexChanged += new System.EventHandler(this.provinceComboBox_SelectedIndexChanged);
+            // 
+            // caseInputTextBox
+            // 
+            this.caseInputTextBox.Location = new System.Drawing.Point(110, 374);
+            this.caseInputTextBox.Name = "caseInputTextBox";
+            this.caseInputTextBox.Size = new System.Drawing.Size(100, 26);
+            this.caseInputTextBox.TabIndex = 6;
+            this.caseInputTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.caseInputTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.caseInputTextBox_KeyDown);
+            // 
+            // Mapa_ni_Tiquia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.caseInputTextBox);
+            this.Controls.Add(this.provinceComboBox);
+            this.Controls.Add(this.regionComboBox);
+            this.Controls.Add(this.islandComboBox);
+            this.Controls.Add(this.islandLabel);
+            this.Controls.Add(this.regionLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.provinceLabel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Mapa_ni_Tiquia";
             this.Text = "Mapa_ni_Tiquia";
+            this.Load += new System.EventHandler(this.Mapa_ni_Tiquia_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mapImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox mapImage;
+        private System.Windows.Forms.Label islandLabel;
+        private System.Windows.Forms.Label regionLabel;
+        private System.Windows.Forms.Label provinceLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox islandComboBox;
+        private System.Windows.Forms.ComboBox regionComboBox;
+        private System.Windows.Forms.ComboBox provinceComboBox;
+        private System.Windows.Forms.TextBox caseInputTextBox;
     }
 }
--- PBL/Mapa ni Tiquia.cs
+++ PBL/Mapa ni Tiquia.cs
@@ -7,14 +7,358 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace PBL
 {
     public partial class Mapa_ni_Tiquia : Form
     {
+        string[] islandList = { "Luzon", "Visayas", "Mindanao" };
+        string[] ncrCity =
+        {
+            "Caloocan City", "Las Pinas City", "Makati City", "Malabon City", "Mandaluyong City", "Manila City", "Marikina City",
+            "Muntinlupa City", "Navotas City", "Paranaque City", "Pasay City", "Pasig City", "Quezon City", "San Juan City", "Taguig City", "Valenzuela City"
+        };
+        string[,] luzonRegionOneToEight =
+        {
+            // nag multi dimensional array ako
+            {"Region I", "Region II", "Region III", "Region IV-A", "Region IV-B", "Region V", "Cordillera Administrative Region" , "NCR"},
+            {"Ilocos Norte", "Ilocos Sur", "La Union", "Pangasinan", "","","",""},                     // ilocos region or region 1
+            {"Batanes", "Cagayan", "Isabela", "Nueva Vizcaya", "Quirino Province","","",""},           //  region 2 or Cagayan Valley
+            {"Aurora", "Bataan", "Bulacan", "Nueva Ecija", "Pampanga", "Tarlac", "Zambales",""},       //region3 or Central Luzon
+            {"Cavite", "Laguna","Batangas","Rizal", "Quezon","","",""},                                // region 4 a calabarzon 
+            {"Occidental Mindoro", "Oriental Mindoro", "Marinduque", "Romblon", "Palawan", "","",""},  //region 4b mimaropa
+            {"Albay", "Camarines Norte", "Camarines Sur", "Sorsogon","Catanduanes", "Masbate", "",""},    //region 5
+            {"Abra", "Benguet", "Ifugao", "Kalinga", "Apayao", "Mountain Province", "",""},    //cordilierra administrative region
+        };
+        string[,] visayasRegionSixToEight =
+        {
+            {"Region VI", "Region VII", "Region VIII", "", "", ""},
+            {"Aklan", "Antique", "Capiz", "Guimaras", "Iloilo", "Negros Occidental" },  // western visayas Regionn 6
+            {"Cebu", "Bohol", "Negros Oriental", "Siquijor", "", "" }, // central visayas region 7
+            {"Leyte", "Biliran", "Southern Leyte", "Samar", "Eastern Samar", "Northern Samar"  }, // eastern visayas
+        };
+
+        string[,] mindanaoRegions =
+        {
+            {"Region IX", "Region X", "Region XI", "Region XII","Region XIII", "BARMM" },
+            {"Zamboanga del Norte", "Zamboanga del Sur", "Zamboanga Sibugay", "", "", "" }, //Zamboanga Peninsula Region 9
+            {"Misamis Oriental", "Misamis Occidental", "Bukidnon", "Camiguin", "Lanao del Norte", "" },         // region 10 northern mindanao
+            {"Davao de Oro", "Davao del Norte", "Davao del Sur", "Davao Oriental", "Davao Occidental",""},      // region 11 southern mindanao dabao region
+            {"South Cotabato", "Cotabato", "Sultan Kudarat", "Sarangani", "", "" },         //central mindanao sooccsargen 12
+            {"Agusan del Norte", "Agusan del Sur", "Surigao del Norte", "Surigao del Sur", "Dinagat Islands", "" }, // region 13 caraga
+            {"Lanao del Sur", "Maguindanao", "Sulu", "Tawi-tawi", "", ""},                  // BARMM
+        };
         public Mapa_ni_Tiquia()
         {
             InitializeComponent();
+        }
+
+        public void populateIsland()
+        {
+            for (int i = 0; i < islandList.Length;i++)
+            {
+                islandComboBox.Items.Add(islandList[i]);
+            }
+        }
+        public void populateRegions(string island)
+        {
+            int dimension = 0;
+            if (island == "Luzon")
+            {
+                regionComboBox.Items.Clear();
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i <dimension;i++)
+                {
+                    if (luzonRegionOneToEight[0,i] != string.Empty)
+                    {
+                        regionComboBox.Items.Add(luzonRegionOneToEight[0, i]);
+                    }
+                }
+
+            }
+            if (island == "Visayas")
+            {
+                regionComboBox.Items.Clear();
+                dimension = visayasRegionSixToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (visayasRegionSixToEight[0, i] != string.Empty)
+                    {
+                        regionComboBox.Items.Add(visayasRegionSixToEight[0, i]);
+                    }
+                }
+
+            }
+            if (island == "Mindanao")
+            {
+                regionComboBox.Items.Clear();
+                dimension = mindanaoRegions.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (mindanaoRegions[0, i] != string.Empty)
+                    {
+                        regionComboBox.Items.Add(mindanaoRegions[0, i]);
+                    }
+                }
+            }
+        }
+        public void populateProvince(string region)
+        {
+            int dimension = 0;
+            // buonng luzon
+            if (region == "Region I")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0); 
+                for (int i = 0; i <dimension;i++ )
+                {
+                    if (luzonRegionOneToEight[1,i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[1, i]);
+                    }
+                }
+            }
+            if (region == "Region II")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (luzonRegionOneToEight[2, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[2, i]);
+                    }
+                }
+            }
+            if (region == "Region III")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (luzonRegionOneToEight[3, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[3, i]);
+                    }
+                }
+            }
+
+            if (region == "Region IV-A")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (luzonRegionOneToEight[4, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[4, i]);
+                    }
+                }
+            }
+            if (region == "Region IV-B")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (luzonRegionOneToEight[5, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[5, i]);
+                    }
+                }
+            }
+            if (region == "Region V")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (luzonRegionOneToEight[6, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[6, i]);
+                    }
+                }
+            }
+            if (region == "Cordillera Administrative Region")
+            {
+                dimension = luzonRegionOneToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (luzonRegionOneToEight[7, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(luzonRegionOneToEight[7, i]);
+                    }
+                }
+            }
+            // end ng luzon
+            // start ng visayas
+            if (region == "Region VI")
+            {
+                dimension = visayasRegionSixToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (visayasRegionSixToEight[1, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(visayasRegionSixToEight[1, i]);
+                    }
+                }
+            }
+            if (region == "Region VII")
+            {
+                dimension = visayasRegionSixToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (visayasRegionSixToEight[2, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(visayasRegionSixToEight[2, i]);
+                    }
+                }
+            }
+            if (region == "Region VIII")
+            {
+                dimension = visayasRegionSixToEight.GetLength(0);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (visayasRegionSixToEight[3, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(visayasRegionSixToEight[3, i]);
+                    }
+                }
+            }
+            // end ng visayas
+            // start ng sa mindanao
+            // length of column kinukuha kasi 6 column 7 rows e column long kailangan
+            if (region == "Region IX")
+            {
+                dimension = mindanaoRegions.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (mindanaoRegions[1, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(mindanaoRegions[1, i]);
+                    }
+                }
+            }
+            if (region == "Region X")
+            {
+                dimension = mindanaoRegions.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (mindanaoRegions[2, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(mindanaoRegions[2, i]);
+                    }
+                }
+            }
+            if (region == "Region XI")
+            {
+                dimension = mindanaoRegions.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (mindanaoRegions[3, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(mindanaoRegions[3, i]);
+                    }
+                }
+            }
+            if (region == "Region XII")
+            {
+                dimension = mindanaoRegions.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (mindanaoRegions[4, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(mindanaoRegions[4, i]);
+                    }
+                }
+            }
+            if (region == "Region XIII")
+            {
+                dimension = visayasRegionSixToEight.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (mindanaoRegions[5, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(mindanaoRegions[5, i]);
+                    }
+                }
+            }
+            if (region == "BARMM")
+            {
+                dimension = mindanaoRegions.GetLength(1);
+                for (int i = 0; i < dimension; i++)
+                {
+                    if (visayasRegionSixToEight[6, i] != string.Empty)
+                    {
+                        provinceComboBox.Items.Add(mindanaoRegions[6, i]);
+                    }
+                }
+            }
+
+
+
+
+
+        }
+        private void Mapa_ni_Tiquia_Load(object sender, EventArgs e)
+        {
+            populateIsland();
+        }
+
+        private void islandComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedIsland = islandComboBox.GetItemText(islandComboBox.SelectedItem);
+            provinceComboBox.Text = string.Empty;
+            populateRegions(selectedIsland);
+
+        }
+
+        private void provinceComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void regionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedRegion = regionComboBox.GetItemText(regionComboBox.SelectedItem);
+            provinceComboBox.Items.Clear();
+            populateProvince(selectedRegion);
+
+            
+        }
+
+        private void islandComboBox_DropDown(object sender, EventArgs e)
+        {
+            regionComboBox.Text = string.Empty;
+            provinceComboBox.Text = string.Empty;
+        }
+
+        private void regionComboBox_DropDown(object sender, EventArgs e)
+        {
+            provinceComboBox.Text = string.Empty;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void caseInputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool mayLamanIslandRegionAtProvince = islandComboBox.Text != string.Empty && regionComboBox.Text != string.Empty && provinceComboBox.Text != string.Empty;
+            if (e.KeyCode == Keys.Enter && mayLamanIslandRegionAtProvince)
+            {
+                MessageBox.Show("Added user");
+                string[] addedCase = { islandComboBox.Text, ", ",regionComboBox.Text, ", ", provinceComboBox.Text, ", ", caseInputTextBox.Text };
+                StreamWriter addUser = new StreamWriter("C:\\ADDCASE\\newcases.txt", true);
+                for (int i = 0; i < addedCase.Length; i++)
+                {
+                    addUser.Write(addedCase[i]);
+                }
+                addUser.WriteLine();
+                addUser.Close();
+                islandComboBox.Text = regionComboBox.Text = provinceComboBox.Text = caseInputTextBox.Text = string.Empty;
+                islandComboBox.Focus();
+                
+
+
+            }
+         
         }
     }
 }
